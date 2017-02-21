7e86e6e0e3a8b64d5e9bd9a5577ab4ba33f1def7
keisukeyumisashi <tropical.beat.recordz@gmail.com>
2017-02-21 05:41:38 +0000

    dig_tateに戻した

--- db/migrate/20170220053430_create_capsules.rb
+++ db/migrate/20170220053430_create_capsules.rb
@@ -2,7 +2,7 @@ class CreateCapsules < ActiveRecord::Migration[5.0]
   def change
     create_table :capsules do |t|
       t.text :message
-      t.timestamp :dig_date
+      t.timestamp :dig_tate
 
       t.timestamps
     end
