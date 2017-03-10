d2799b467dca2f6f35d36a408452a8e7230265c7
kyokoshima <dev.kyoko@gmail.com>
2017-03-10 13:43:25 +0800

    add *.swp to .gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
   devise_for :users, controllers: {
 	  confirmations: 'confirmations',
 	  invitations: 'invitations'
-
   }
+  resources :users
   root 'pages#index'
 
   get 'pages/show'
