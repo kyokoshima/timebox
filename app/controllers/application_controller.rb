51d8a7e7e72f99eba495a9f5ee3802e079fb2453
sato1122 <sato.fururi@gmail.com>
2017-03-14 06:15:16 +0000

    temporary commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  private
+
+	def after_sing_in_path_for
+			capsules_path
+	end
+
+	def after_sign_out_path_for
+			new_user_session_path
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  resources :users
+  devise_scope :user do
+    get '/sign_out' => 'devise/sessions#destroy'
+  end
+
   devise_for :users, controllers: {
 	  confirmations: 'confirmations',
 	  invitations: 'invitations'
   }
-  resources :users
+
   root 'pages#index'
 
   get 'pages/show'
