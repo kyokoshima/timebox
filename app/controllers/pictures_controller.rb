204a10df63aa7a1a27b0a4d1e452a1aa603e91a4
kyokoshima <dev.kyoko@gmail.com>
2017-03-22 13:53:44 +0800

    fix cant login

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -14,6 +14,7 @@
 
 class PicturesController < ApplicationController
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /pictures
   # GET /pictures.json
